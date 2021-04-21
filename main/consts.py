48a6400c30bb5355087a36124b8c33486d623a5f
Koen Deschacht <koendeschacht@gmail.com>
2021-04-21 17:25:24 +0200

    Let's try dailies?

--- main/consts.py
+++ main/consts.py
@@ -7,8 +7,8 @@ BACKFILL_SECONDS = 7776000  # Maximum allowed by the API
 BACKFILL_SLEEP_BETWEEN_CALLS = 2
 
 GARMIN_BACKFILL_URLS = [
-    # 'https://healthapi.garmin.com/wellness-api/rest/backfill/dailies',
-    'https://healthapi.garmin.com/wellness-api/rest/backfill/epochs',
+    'https://healthapi.garmin.com/wellness-api/rest/backfill/dailies',
+    # 'https://healthapi.garmin.com/wellness-api/rest/backfill/epochs',
     # 'https://healthapi.garmin.com/wellness-api/rest/backfill/sleeps',
     # 'https://healthapi.garmin.com/wellness-api/rest/backfill/bodyComps',
     # 'https://healthapi.garmin.com/wellness-api/rest/backfill/stressDetails',
